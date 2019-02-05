3fa44d414d715f973072d650bb230ee596ce396b
Latz <filip.lackovic@undabot.com>
2019-02-05 14:03:14 +0100

    csfix

--- src/Encoding/RelationshipToPhpArrayEncoderInterface.php
+++ src/Encoding/RelationshipToPhpArrayEncoderInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Undabot\JsonApi\Encoding;
 
-
 use Undabot\JsonApi\Model\Resource\Relationship\RelationshipInterface;
 
 interface RelationshipToPhpArrayEncoderInterface
--- src/Encoding/ResourceIdentifierToPhpArrayEncoderInterface.php
+++ src/Encoding/ResourceIdentifierToPhpArrayEncoderInterface.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 namespace Undabot\JsonApi\Encoding;
+
 use Undabot\JsonApi\Model\Resource\ResourceIdentifierInterface;
 
 interface ResourceIdentifierToPhpArrayEncoderInterface
