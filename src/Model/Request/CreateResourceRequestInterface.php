3375a14aad0858bd75e7423d9fbb6a99f15d7991
Zoran Antolović <zoran.antolovic@undabot.com>
2019-09-04 09:29:01 +0200

    Remove unused import statement

--- src/Model/Request/CreateResourceRequestInterface.php
+++ src/Model/Request/CreateResourceRequestInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Undabot\JsonApi\Model\Request;
 
-use Undabot\JsonApi\Model\Resource\ResourceInterface;
-
 interface CreateResourceRequestInterface extends ResourcePayloadRequest
 {
 }
