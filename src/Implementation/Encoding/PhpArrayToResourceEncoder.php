6d347f9df67b294cffd9264e6c23088c4be3a8cc
Marko Plesa <marko.plesa@sintezis.co>
2023-10-26 13:35:27 +0200

    feat: move assertStringKeyArray and assertStringKeyArrayNested functions to ArrayUtil

--- src/Implementation/Encoding/PhpArrayToResourceEncoder.php
+++ src/Implementation/Encoding/PhpArrayToResourceEncoder.php
@@ -12,6 +12,7 @@ use Undabot\JsonApi\Definition\Model\Meta\MetaInterface;
 use Undabot\JsonApi\Definition\Model\Resource\ResourceInterface;
 use Undabot\JsonApi\Implementation\Encoding\Exception\JsonApiEncodingException;
 use Undabot\JsonApi\Implementation\Model\Resource\Resource;
+use Undabot\JsonApi\Util\ArrayUtil;
 use Undabot\JsonApi\Util\Exception\ValidationException;
 use Undabot\JsonApi\Util\ValidResourceAssertion;
 
@@ -19,13 +20,13 @@ use Undabot\JsonApi\Util\ValidResourceAssertion;
 class PhpArrayToResourceEncoder implements PhpArrayToResourceEncoderInterface
 {
     /** @var PhpArrayToRelationshipCollectionEncoderInterface */
-    private $phpArrayToRelationshipCollectionEncoder;
+    private PhpArrayToRelationshipCollectionEncoderInterface $phpArrayToRelationshipCollectionEncoder;
 
     /** @var PhpArrayToAttributeCollectionEncoderInterface */
-    private $phpArrayToAttributeCollectionEncoder;
+    private PhpArrayToAttributeCollectionEncoderInterface $phpArrayToAttributeCollectionEncoder;
 
     /** @var PhpArrayToMetaEncoderInterface */
-    private $phpArrayToMetaEncoder;
+    private PhpArrayToMetaEncoderInterface $phpArrayToMetaEncoder;
 
     public function __construct(
         PhpArrayToRelationshipCollectionEncoderInterface $phpArrayToRelationshipCollectionEncoder,
@@ -54,10 +55,10 @@ class PhpArrayToResourceEncoder implements PhpArrayToResourceEncoderInterface
         }
 
 
-        $rawAttributes = $this->assertStringKeyArray($resource, 'attributes');
-        $rawMeta = $this->assertStringKeyArray($resource, 'meta');
+        $rawAttributes = ArrayUtil::assertStringKeyArray($resource, 'attributes');
+        $rawMeta = ArrayUtil::assertStringKeyArray($resource, 'meta');
         $rawLink = is_array($resource['links'] ?? null) ? ($resource['links']['self'] ?? null) : null;
-        $rawRelationships = $this->assertStringKeyArrayNested($resource, 'relationships');
+        $rawRelationships = ArrayUtil::assertStringKeyArrayNested($resource, 'relationships');
 
         if (null !== $rawLink) {
             throw new \RuntimeException('Not implemented');
@@ -86,83 +87,5 @@ class PhpArrayToResourceEncoder implements PhpArrayToResourceEncoderInterface
         }
 
         return $this->phpArrayToMetaEncoder->decode($rawMeta);
-    }
-
-//    /**
-//     *
-//     * @param mixed $array
-//     * @return array<string, mixed>
-//     */
-//    private function assertStringKeyArray(mixed $array): array
-//    {
-//        if (!is_array($array)) {
-//            throw new \InvalidArgumentException("Parameter is not array.");
-//        }
-//        foreach ($array as $key) {
-//            if (!is_string($key)) {
-//                throw new \InvalidArgumentException("Array key must be a string.");
-//            }
-//        }
-//
-//        return $array;
-//
-//    }
-
-    /**
-     * Asserts and returns an array with string keys. If the key does not exist in the source array,
-     * it returns an empty array or a specified default value.
-     *
-     * @param array<mixed> $source The source array from which to extract the value.
-     * @param string $key The key to look for in the source array.
-     * @param mixed $default The default value to return if the key is not found. Defaults to an empty array.
-     * @return array<string, mixed> The asserted array with string keys and mixed values.
-     */
-    private function assertStringKeyArray(array $source, string $key, $default = []): array
-    {
-        $array = $source[$key] ?? $default;
-
-        if (!is_array($array)) {
-            throw new \InvalidArgumentException("The value for '{$key}' is not an array.");
-        }
-
-        foreach (array_keys($array) as $key) {
-            if (!is_string($key)) {
-                throw new \InvalidArgumentException("Array key must be a string.");
-            }
-        }
-
-        return $array;
-    }
-
-    /**
-     * Asserts and returns a nested array with string keys. If the key does not exist in the source array,
-     * it returns an empty array or a specified default value.
-     *
-     * @param array<mixed> $source The source array from which to extract the value.
-     * @param string $key The key to look for in the source array.
-     * @param mixed $default The default value to return if the key is not found. Defaults to an empty array.
-     * @return array<string, array<string, mixed>> The asserted nested array with string keys at both levels.
-     */
-    private function assertStringKeyArrayNested(array $source, string $key, $default = []): array
-    {
-        $array = $source[$key] ?? $default;
-
-        if (!is_array($array)) {
-            throw new \InvalidArgumentException("The value for '{$key}' is not an array.");
-        }
-
-        foreach ($array as $topKey => $nestedArray) {
-            if (!is_string($topKey)) {
-                throw new \InvalidArgumentException("Top-level key must be a string.");
-            }
-
-            if (!is_array($nestedArray)) {
-                throw new \InvalidArgumentException("Value under '{$topKey}' must be an array.");
-            }
-
-            $array[$topKey] = $this->assertStringKeyArray($nestedArray, $key);
-        }
-
-        return $array;
     }
 }
--- src/Util/ArrayUtil.php
+++ src/Util/ArrayUtil.php
@@ -37,4 +37,62 @@ abstract class ArrayUtil
 
         return $keys === $nonIntKeys;
     }
+
+    /**
+     * Asserts and returns an array with string keys. If the key does not exist in the source array,
+     * it returns an empty array or a specified default value.
+     *
+     * @param array<mixed> $source The source array from which to extract the value.
+     * @param string $key The key to look for in the source array.
+     * @param mixed $default The default value to return if the key is not found. Defaults to an empty array.
+     * @return array<string, mixed> The asserted array with string keys and mixed values.
+     */
+    public static function assertStringKeyArray(array $source, string $key, $default = []): array
+    {
+        $array = $source[$key] ?? $default;
+
+        if (!is_array($array)) {
+            throw new \InvalidArgumentException("The value for '{$key}' is not an array.");
+        }
+
+        foreach (array_keys($array) as $key) {
+            if (!is_string($key)) {
+                throw new \InvalidArgumentException("Array key must be a string.");
+            }
+        }
+
+        return $array;
+    }
+
+    /**
+     * Asserts and returns a nested array with string keys. If the key does not exist in the source array,
+     * it returns an empty array or a specified default value.
+     *
+     * @param array<mixed> $source The source array from which to extract the value.
+     * @param string $key The key to look for in the source array.
+     * @param mixed $default The default value to return if the key is not found. Defaults to an empty array.
+     * @return array<string, array<string, mixed>> The asserted nested array with string keys at both levels.
+     */
+    public static function assertStringKeyArrayNested(array $source, string $key, $default = []): array
+    {
+        $array = $source[$key] ?? $default;
+
+        if (!is_array($array)) {
+            throw new \InvalidArgumentException("The value for '{$key}' is not an array.");
+        }
+
+        foreach ($array as $topKey => $nestedArray) {
+            if (!is_string($topKey)) {
+                throw new \InvalidArgumentException("Top-level key must be a string.");
+            }
+
+            if (!is_array($nestedArray)) {
+                throw new \InvalidArgumentException("Value under '{$topKey}' must be an array.");
+            }
+
+            $array[$topKey] = ArrayUtil::assertStringKeyArray($nestedArray, $key);
+        }
+
+        return $array;
+    }
 }
